1aa0780f36e71707eee1f95ea5c27b6d0c76bd46
grookee <eatpiern@proton.me>
2025-06-09 02:11:00 +0300

    feat: needed session and bancho services

---
+++ lib/green_elixir/services/beatmap_service.ex
@@ -0,0 +1,79 @@
+defmodule GreenElixir.Services.BeatmapService do
+  @moduledoc """
+  Manages beatmap data, integrates with Observatory API.
+  """
+  use GenServer
+
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
+  end
+
+  def get_beatmap_set(beatmap_hash, beatmap_id \\ nil) do
+    GenServer.call(__MODULE__, {:get_beatmap_set, beatmap_hash, beatmap_id})
+  end
+
+  def get_beatmap_difficulty(beatmap_id, mods \\ 0) do
+    GenServer.call(__MODULE__, {:get_difficulty, beatmap_id, mods})
+  end
+
+  @impl true
+  def init(_opts) do
+    observatory_url =
+      Application.get_env(:Green_elixir, :observatory_url, "http://localhost:5000")
+
+    {:ok, %{observatory_url: observatory_url}}
+  end
+
+  @impl true
+  def handle_call({:get_beatmap_set, beatmap_hash, beatmap_id}, _from, state) do
+    case fetch_from_observatory(state.observatory_url, beatmap_hash, beatmap_id) do
+      {:ok, beatmap_data} ->
+        # Cache the beatmap data
+        cache_beatmap_data(beatmap_data)
+        {:reply, {:ok, beatmap_data}, state}
+
+      {:error, reason} ->
+        {:reply, {:error, reason}, state}
+    end
+  end
+
+  @impl true
+  def handle_call({:get_difficulty, beatmap_id, mods}, _from, state) do
+    case calculate_difficulty_with_mods(beatmap_id, mods) do
+      {:ok, difficulty} -> {:reply, {:ok, difficulty}, state}
+      {:error, reason} -> {:reply, {:error, reason}, state}
+    end
+  end
+
+  defp fetch_from_observatory(base_url, beatmap_hash, beatmap_id) do
+    url = "#{base_url}/api/beatmap"
+    params = if beatmap_id, do: %{id: beatmap_id}, else: %{hash: beatmap_hash}
+
+    case HTTPoison.get(url, [], params: params) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        {:ok, Jason.decode!(body)}
+
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        {:error, :beatmap_not_found}
+
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+
+  defp calculate_difficulty_with_mods(beatmap_id, mods) do
+    # Implement difficulty calculation with mods
+    # This would typically call a difficulty calculator service
+    {:ok,
+     %{
+       aim_difficulty: 5.0,
+       speed_difficulty: 5.0,
+       overall_difficulty: 8.0,
+       max_combo: 1000
+     }}
+  end
+
+  defp cache_beatmap_data(beatmap_data) do
+    Cachex.put(:Green_cache, "beatmap:#{beatmap_data["id"]}", beatmap_data, ttl: :timer.hours(1))
+  end
+end
---
+++ lib/green_elixir/services/session_manager.ex
@@ -0,0 +1,146 @@
+defmodule GreenElixir.Services.SessionManager do
+  use GenServer
+  alias GreenElixir.Accounts
+  alias GreenElixir.Bancho.Protocol
+
+  defstruct [:user_id, :username, :token, :socket, :country, :action, :last_ping, :match_id]
+
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
+  end
+
+  def create_session(user_id, username, socket) do
+    GenServer.call(__MODULE__, {:create_session, user_id, username, socket})
+  end
+
+  def get_session(token) do
+    GenServer.call(__MODULE__, {:get_session, token})
+  end
+
+  def update_session(token, updates) do
+    GenServer.call(__MODULE__, {:update_session, token, updates})
+  end
+
+  def remove_session(token) do
+    GenServer.call(__MODULE__, {:remove_session, token})
+  end
+
+  def list_online_users do
+    GenServer.call(__MODULE__, :list_online_users)
+  end
+
+  def broadcast_to_all(packet_type, data, exclude \\ []) do
+    GenServer.cast(__MODULE__, {:broadcast_to_all, packet_type, data, exclude})
+  end
+
+  @impl true
+  def init(_opts) do
+    :ets.new(:sessions, [:set, :protected, :named_table])
+    :ets.new(:user_sessions, [:set, :protected, :named_table])
+    {:ok, %{}}
+  end
+
+  @impl true
+  def handle_call({:create_session, user_id, username, socket}, _from, state) do
+    token = generate_token()
+
+    session = %__MODULE__{
+      user_id: user_id,
+      username: username,
+      token: token,
+      socket: socket,
+      last_ping: System.system_time(:second)
+    }
+
+    :ets.insert(:sessions, {token, session})
+    :ets.insert(:user_sessions, {user_id, token})
+
+    # Notify other users of new user online
+    broadcast_user_presence(session)
+
+    {:reply, {:ok, token}, state}
+  end
+
+  @impl true
+  def handle_call({:get_session, token}, _from, state) do
+    case :ets.lookup(:sessions, token) do
+      [{_token, session}] -> {:reply, {:ok, session}, state}
+      [] -> {:reply, {:error, :not_found}, state}
+    end
+  end
+
+  @impl true
+  def handle_call({:update_session, token, updates}, _from, state) do
+    case :ets.lookup(:sessions, token) do
+      [{_token, session}] ->
+        updated_session = struct(session, updates)
+        :ets.insert(:sessions, {token, updated_session})
+        {:reply, :ok, state}
+
+      [] ->
+        {:reply, {:error, :not_found}, state}
+    end
+  end
+
+  @impl true
+  def handle_call({:remove_session, token}, _from, state) do
+    case :ets.lookup(:sessions, token) do
+      [{_token, session}] ->
+        :ets.delete(:sessions, token)
+        :ets.delete(:user_sessions, session.user_id)
+
+        # Notify other users that user went offline
+        broadcast_user_logout(session)
+
+        {:reply, :ok, state}
+
+      [] ->
+        {:reply, {:error, :not_found}, state}
+    end
+  end
+
+  @impl true
+  def handle_call(:list_online_users, _from, state) do
+    users =
+      :ets.tab2list(:sessions)
+      |> Enum.map(fn {_token, session} -> session end)
+
+    {:reply, users, state}
+  end
+
+  @impl true
+  def handle_cast({:broadcast_to_all, packet_type, data, exclude}, state) do
+    :ets.tab2list(:sessions)
+    |> Enum.each(fn {_token, session} ->
+      unless session.user_id in exclude do
+        send_packet_to_session(session, packet_type, data)
+      end
+    end)
+
+    {:noreply, state}
+  end
+
+  defp broadcast_user_presence(session) do
+    presence_data = %{
+      user_id: session.user_id,
+      username: session.username,
+      country: session.country || 0
+    }
+
+    broadcast_to_all(:server_user_presence, presence_data, [session.user_id])
+  end
+
+  defp broadcast_user_logout(session) do
+    logout_data = %{user_id: session.user_id}
+    broadcast_to_all(:server_user_logout, logout_data)
+  end
+
+  defp send_packet_to_session(session, packet_type, data) do
+    packet = Protocol.encode_packet(packet_type, data)
+    send(session.socket, {:send_data, packet})
+  end
+
+  defp generate_token do
+    :crypto.strong_rand_bytes(32) |> Base.encode64()
+  end
+end
